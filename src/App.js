3db5a11ca49af1d54ace638b081eb1529410c8f3
kocaman14 <o.kcmn@hotmail.com>
2024-09-06 16:04:08 +0300

    Problem01

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Head from './head'
+import Form from './Form';
+
+// App/Header/Form bileşenlerine ayırın, kendi adlarıyla ayrı dosyalara koyun
+
+export default function App() {
+  return (
+    <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+     <Head />
+     <Form />
+    </div>
+  )
+}
